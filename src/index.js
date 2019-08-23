6258c3ed4612ef6afdb3a4fbd63058ff62edcc8a
Jackson Meyer-Lee <jmeyerlee@axio.com>
2019-08-23 13:29:31 -0400

    show/hide function

--- src/index.js
+++ src/index.js
@@ -4,10 +4,29 @@ import { VictoryExample } from "./victory";
 
 import "./styles.css";
 
+class ShowChartButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {show: false}
+  }
+
+
+  render = () =>
+  <>
+    <div className={"showHideButton"}>
+      <input
+        type={"button"}
+        value={"show the chart"}
+        onClick={() => this.setState({show: !this.state.show})}
+        />
+    </div>
+    {this.state.show && <VictoryExample/>}
+  </>;
+}
 function App() {
   return (
     <div className="App">
-      <VictoryExample />
+      <ShowChartButton/>
     </div>
   );
 }
